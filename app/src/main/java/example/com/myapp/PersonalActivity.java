357e623f9b040b4c53dad183ea80d360e0eceed2
shivangiGotwala <shivangimarwadi3375@gmail.com>
2019-09-13 15:59:31 -0700

    Changes in Personal Activity

--- app/src/main/java/example/com/myapp/PersonalActivity.java
+++ app/src/main/java/example/com/myapp/PersonalActivity.java
@@ -2,6 +2,10 @@ package example.com.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PersonalActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class PersonalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+
+        final EditText edtOne = findViewById(R.id.edt_one);
+        final EditText edtTwo = findViewById(R.id.edt_two);
+        final TextView tvSum = findViewById(R.id.tv_sum);
+        Button btnAdd = findViewById(R.id.add);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int sum = Integer.parseInt(edtOne.getText().toString()) + Integer.parseInt(edtTwo.getText().toString());
+                tvSum.setText(String.valueOf(sum));
+            }
+        });
+
     }
 }

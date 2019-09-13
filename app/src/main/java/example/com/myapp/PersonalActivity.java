dd2658de8b59149aba7f72c6c619c22f91cc0bb0
shivangiGotwala <shivangimarwadi3375@gmail.com>
2019-09-13 15:45:11 -0700

    Commit

--- app/src/main/java/example/com/myapp/PersonalActivity.java
+++ app/src/main/java/example/com/myapp/PersonalActivity.java
@@ -2,16 +2,12 @@ package example.com.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class PersonalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn1 = findViewById(R.id.btn1);
-        Button btn2 = findViewById(R.id.btn2);
-
+        setContentView(R.layout.activity_personal);
     }
 }
